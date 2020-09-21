a81e2274dc603c16cab143bb646c183b2c369fde
Chriscastanuela <chriscastanuela@ymail.com>
2020-09-21 07:36:32 -0600

    Add cards for empty trip sections showing the user that they don't have trips in that section

--- src/index.js
+++ src/index.js
@@ -22,10 +22,10 @@ let presentHeader = document.querySelector('.Present-Header');
 let upcomingHeader = document.querySelector('.Upcoming-Header');
 let pendingHeader = document.querySelector('.Pending-Header');
 
-let pastText = document.querySelector('#Past-Text');
-let presentText = document.querySelector('#Present-Text');
-let upcomingText = document.querySelector('#Upcoming-Text');
-let pendingText = document.querySelector('#Pending-Text');
+// let pastText = document.querySelector('#Past-Text');
+// let presentText = document.querySelector('#Present-Text');
+// let upcomingText = document.querySelector('#Upcoming-Text');
+// let pendingText = document.querySelector('#Pending-Text');
 
 // <-------------------------------------------->Class Declarations
 let traveler;
@@ -42,14 +42,14 @@ function getDataAndShowDom() {
         traveler.getPersonalInfo(),
         traveler.getTripData()
     ])
-    .then(fetches => {
-        console.log(fetches[0].firstName);
+    .then(promiseDotAllIndex => {
+        console.log(promiseDotAllIndex[0].firstName);
         domInfo();
     })
 }
 
 function createClasses() {
-    traveler = new Traveler(7);
+    traveler = new Traveler(9);
     console.log(traveler);
 }
 
@@ -58,9 +58,13 @@ function domInfo() {
     greeting.innerHTML = `Welcome back, ${traveler.firstName} the ${traveler.travelerType}!`;
     userFullName.innerHTML = `${traveler.fullName}`;
     expenses.innerHTML = `YTD Travel Expenses: `;
-
-    traveler.pastTrips.forEach(element => {
-        if (traveler.pastTrips.length > 0) {
+    if (traveler.pastTrips.length == 0) {
+        pastHeader.insertAdjacentHTML(`afterend`, `<div class="Trip-Div" id="Past-Div">
+        <p class="Trip-Div-Text" id="Past-Text">
+        You don't have any trips in this section</p>
+      </div>`)
+    } else {
+        traveler.pastTrips.forEach(element => {
             pastHeader.insertAdjacentHTML(`afterend`, `<div class="Trip-Div" id="Past-Div">
             <p class="Trip-Div-Text" id="Past-Text">
             Destination: ${element.destinationID}<br>
@@ -69,11 +73,16 @@ function domInfo() {
             Duration: ${element.duration} days<br>
             Travelers on board: ${element.travelers}<br>
             </p>
-          </div>`)
-        }
-    });
-    traveler.currentTrips.forEach(element => {
-        if (traveler.currentTrips.length > 0) {
+            </div>`)
+        });
+    }
+    if (traveler.currentTrips.length == 0) {
+        presentHeader.insertAdjacentHTML(`afterend`, `<div class="Trip-Div" id="Present-Div">
+        <p class="Trip-Div-Text" id="Present-Text">
+        You don't have any trips in this section</p>
+      </div>`)
+    } else {
+        traveler.currentTrips.forEach(element => {
             presentHeader.insertAdjacentHTML(`afterend`, `<div class="Trip-Div" id="Present-Div">
             <p class="Trip-Div-Text" id="Present-Text">
             Destination: ${element.destinationID}<br>
@@ -82,11 +91,16 @@ function domInfo() {
             Duration: ${element.duration} days<br>
             Travelers on board: ${element.travelers}<br>
             </p>
-          </div>`)
-        }
-    });
-    traveler.futureTrips.forEach(element => {
-        if (traveler.futureTrips.length > 0) {
+            </div>`)
+        });
+    }
+    if (traveler.futureTrips.length == 0) {
+        upcomingHeader.insertAdjacentHTML(`afterend`, `<div class="Trip-Div" id="Upcoming-Div">
+        <p class="Trip-Div-Text" id="Upcoming-Text">
+        You don't have any trips in this section</p>
+      </div>`)
+    } else {
+        traveler.futureTrips.forEach(element => {
             upcomingHeader.insertAdjacentHTML(`afterend`, `<div class="Trip-Div" id="Upcoming-Div">
             <p class="Trip-Div-Text" id="Upcoming-Text">
             Destination: ${element.destinationID}<br>
@@ -95,11 +109,16 @@ function domInfo() {
             Duration: ${element.duration} days<br>
             Travelers on board: ${element.travelers}<br>
             </p>
-          </div>`)
-        }
-    });
-    traveler.pendingTrips.forEach(element => {
-        if (traveler.pendingTrips.length > 0) {
+            </div>`)
+        });
+    }
+    if (traveler.pendingTrips.length == 0) {
+        pendingHeader.insertAdjacentHTML(`afterend`, `<div class="Trip-Div" id="Pending-Div">
+        <p class="Trip-Div-Text" id="Pending-Text">
+        You don't have any trips in this section</p>
+      </div>`)
+    } else {
+        traveler.pendingTrips.forEach(element => {
             pendingHeader.insertAdjacentHTML(`afterend`, `<div class="Trip-Div" id="Pending-Div">
             <p class="Trip-Div-Text" id="Pending-Text">
             Destination: ${element.destinationID}<br>
@@ -108,9 +127,9 @@ function domInfo() {
             Duration: ${element.duration} days<br>
             Travelers on board: ${element.travelers}<br>
             </p>
-          </div>`)
-        }
-    });
+            </div>`)
+        });
+    }
 }
 
 // 
